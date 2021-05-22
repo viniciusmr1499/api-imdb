3e2c493d30f4023dce25cbd2bf293998d5429dd8
Marcos  Oliveira <viniciusmr1499@gmail.com>
2021-05-21 21:59:45 -0300

    alterado servico que retorna todos os usuarios

--- src/modules/users/services/GetUserService.ts
+++ src/modules/users/services/GetUserService.ts
@@ -2,7 +2,6 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import { inject, injectable } from 'tsyringe'
 
 import User from '@modules/users/infra/typeorm/entities/User'
-import AppError from '@shared/errors/AppError'
 
 @injectable()
 class GetUserService {
